d18566fb82df495d9fc46febf8542796e63fe355
0xMRTT <0xMRTT@proton.me>
2023-06-08 22:31:53 +0200

    fix: rm run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-cd /home/0xmrtt/matrixai
-
-export MATRIX_PASSWORD="Y77GSrazfsvtf5U9rNe8gu5bk4aDSsh5QQxJE2pVmtnxyZ6DXAeR764W9QtkgyB"
-export AI_COMMAND_ALIASES="gpt, ask"
-
-python matrixai/__init__.py
-
